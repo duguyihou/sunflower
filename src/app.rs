a14ad945e914f3d62762366120701debd641a3c1
kong <duguyihou@gmail.com>
2022-10-14 12:06:02 +1100

    feat: install yew

---
+++ src/app.rs
@@ -0,0 +1,10 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+      <main>
+        <h1>{ "Hello Sunflower!" }</h1>
+      </main>
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod app;
+
+use app::App;
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
 }
